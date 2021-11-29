51aa00ecab0713c84b0b127a7bfd0fc59b96838f
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2021-11-29 18:22:10 -0500

    6.22

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+
+//conectarDB();
+
 app.use(express.static('./dist/FerreTic'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/FerreTic/'}),
 );
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
