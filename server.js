75d35acbbfa0994c57cd5bea62da941aee2d422e
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2021-11-29 18:32:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const path = require('path');
 
 const app = express();
 
+
+//dbmongo
+conectarDB();
+app.use(cors());
+app.use(express.json());
+app.use('/api/productos', require('./routes/producto'));
+
 // Serve only the static files form the dist directory
-
-//conectarDB();
-
 app.use(express.static('./dist/FerreTic'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/FerreTic/'}),
 );
 
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
 
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, host, () => {
+    console.console.log('El servidor esta funcionando');
+});
