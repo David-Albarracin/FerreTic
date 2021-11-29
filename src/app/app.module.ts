379c625de8536b8aa229907aadd9944d53d44cb2
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2021-11-29 12:38:06 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { HttpClientModule } from '@angular/common/http';
-
-// Componentes
 import { AppComponent } from './app.component';
-import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
-import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CrearProductoComponent,
-    ListarProductosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
