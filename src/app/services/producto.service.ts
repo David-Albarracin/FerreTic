1223e02533c717637eb74194b1daef5023435868
David-Albarracin <90213684+David-Albarracin@users.noreply.github.com>
2021-11-29 20:58:24 -0500

    Update producto.service.ts

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductoService {
-  url = 'http://ferretic.herokuapp.com:8080/api/productos/';
+  url = 'http://ferretic.herokuapp.com/api/productos/';
 
   constructor(private http: HttpClient) { }
 
